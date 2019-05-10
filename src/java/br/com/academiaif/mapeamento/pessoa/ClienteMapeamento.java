d1581dabe63f9825bd4779a281bc71c035332d67
frank pinheiro <frankcpa84@gmail.com>
2019-05-10 09:11:33 -0400

    Criação da classe cargo e mapeamento da cliente e funcionario

--- src/java/br/com/academiaif/mapeamento/pessoa/ClienteMapeamento.java
+++ src/java/br/com/academiaif/mapeamento/pessoa/ClienteMapeamento.java
@@ -7,12 +7,36 @@ package br.com.academiaif.mapeamento.pessoa;
 
 import br.com.academiaif.mampeamento.outros.PlanoMapeamento;
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  *
  * @author Frank
  */
+@Entity
+@Table(name = "clientes")
 public class ClienteMapeamento extends PessoaMapeamento implements Serializable{
     private PlanoMapeamento planoMapeamento;
+    @Column(nullable = false)
     private int diaVencimento;
+
+    public PlanoMapeamento getPlanoMapeamento() {
+        return planoMapeamento;
+    }
+
+    public void setPlanoMapeamento(PlanoMapeamento planoMapeamento) {
+        this.planoMapeamento = planoMapeamento;
+    }
+
+    public int getDiaVencimento() {
+        return diaVencimento;
+    }
+
+    public void setDiaVencimento(int diaVencimento) {
+        this.diaVencimento = diaVencimento;
+    }
+    
+    
 }
--- src/java/br/com/academiaif/mapeamento/pessoa/FuncionarioMapeamento.java
+++ src/java/br/com/academiaif/mapeamento/pessoa/FuncionarioMapeamento.java
@@ -5,10 +5,38 @@
  */
 package br.com.academiaif.mapeamento.pessoa;
 
+import br.com.academiaif.mampeamento.outros.CargoMapeamento;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Frank
  */
-public class FuncionarioMapeamento {
+@Entity
+@Table(name = "funcionarios")
+public class FuncionarioMapeamento extends PessoaMapeamento implements Serializable{
+    private CargoMapeamento cargoMapeamento;
+    private int satisfacaoDoCliente;
+
+    public CargoMapeamento getCargoMapeamento() {
+        return cargoMapeamento;
+    }
+
+    public void setCargoMapeamento(CargoMapeamento cargoMapeamento) {
+        this.cargoMapeamento = cargoMapeamento;
+    }
+
+    public int getSatisfacaoDoCliente() {
+        return satisfacaoDoCliente;
+    }
+
+    public void setSatisfacaoDoCliente(int satisfacaoDoCliente) {
+        this.satisfacaoDoCliente = satisfacaoDoCliente;
+    }
+    
     
 }
